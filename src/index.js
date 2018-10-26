0603d25fa4802f2cf0f3c9f6743a763b78e665d2
Neal Malkani <Neal.Malkani@washpost.com>
2018-10-26 12:55:22 -0400

    linting fixes

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ export class ArcAds {
     if ((isMobile.any() && display === 'mobile') || (!isMobile.any() && display === 'desktop') || (display === 'all')) {
       // Registers the advertisement with Prebid.js if enabled on both the unit and wrapper.
       if ((bidding.prebid && bidding.prebid.bids) && (this.wrapper.prebid && this.wrapper.prebid.enabled) && flatDimensions) {
-        if(pbjs && iframeBidders.length > 0){
+        if (pbjs && iframeBidders.length > 0) {
           pbjs.setConfig({
             userSync: {
               iframeEnabled: true,
