8f792cbb443b1777e371bd903f3da20c39f4a6b7
Neal Malkani <Neal.Malkani@washpost.com>
2019-01-11 14:27:06 -0500

    defensive check

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ export class ArcAds {
     const flatDimensions = [];
     let processDisplayAd = false;
 
-    if (typeof dimensions[0] === 'number') {
+    if (dimensions && typeof dimensions !== 'undefined' && typeof dimensions[0] === 'number') {
       flatDimensions.push(...dimensions);
     } else if (dimensions && typeof dimensions !== 'undefined' && dimensions.length > 0 && dimensions[0][0][0] === undefined) {
       flatDimensions.push(...dimensions);
