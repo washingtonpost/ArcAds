2fb9d0605c9dd101388ffcbcc834414b97bea636
Rachel Myers <rachel.myers@washpost.com>
2020-09-09 15:22:01 -0400

    query.js tests

---
+++ src/__tests__/query.test.js
@@ -0,0 +1,34 @@
+import { expandQueryString } from '../util/query.js';
+
+
+describe('expandQueryString', () => {
+
+    const saveLocation = global.window.location;
+
+    afterAll(() => {
+        delete global.window.location;
+        global.window.location = saveLocation;
+    });
+
+    it('gets url value for param name passed', () => {
+        delete global.window.location;
+        global.window = Object.create(window);
+        global.window.location = {
+          href:'http://www.test.com?adslot=hello',
+        };
+
+      const result = expandQueryString('adslot');
+      expect(result).toEqual('hello');
+    });
+
+    it('if no result return empty string', () => {
+        delete global.window.location;
+        global.window = Object.create(window);
+        global.window.location = {
+          href:'http://www.test.com?adslot=',
+        };
+
+      const result = expandQueryString('adslot');
+      expect(result).toEqual('');
+    });
+  });
