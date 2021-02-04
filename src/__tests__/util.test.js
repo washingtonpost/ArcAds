5f0e88865775dcc0c4ef1b04082d0b3fc151ca46
Evan Brettell <evbrettell@gmail.com>
2021-02-04 13:32:29 -0500

    testing anylogger instead of console

--- src/__tests__/util.test.js
+++ src/__tests__/util.test.js
@@ -1,7 +1,8 @@
+import anylogger from 'anylogger';
+import 'anylogger-console';
 import { renamePositionKey } from '../util/customTargeting';
 import {debounce} from '../util/debounce.js';
 import {sendLog} from '../util/log.js';
-import anylogger from 'anylogger';
 
 describe('The CustomTargeting.js functions', () => {
   it('should take targeting and position value, and rename the key as posn', () => {
@@ -62,11 +63,11 @@ describe('sendLog', () => {
     value: location
   });
 
-  test('sendLog', () => {
+test('sendLog', () => {
     const DATE_TO_USE = new Date('Thu Feb 04 2021 11:04:05 GMT-0500');
     global.Date = jest.fn(() => DATE_TO_USE);
-    global.console.log = jest.fn();
+    anylogger.log = jest.fn();
     sendLog('testFunc()', 'a test of the send log', null);
-    expect(global.console.log).toHaveBeenCalledWith({"description": "a test of the send log", "logging from": "testFunc()", "service": "ArcAds", "slotName": null, "timestamp": "Thu Feb 04 2021 11:04:05 GMT-0500 (Eastern Standard Time)"});
+    expect(anylogger.log).toHaveBeenCalledWith('arcads.js', [{"description": "a test of the send log", "logging from": "testFunc()", "service": "ArcAds", "slotName": null, "timestamp": "Thu Feb 04 2021 11:04:05 GMT-0500 (Eastern Standard Time)"}]);
   });
 });
