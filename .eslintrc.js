73c1d6f6b2b55e7eddb7119201d4323304f88503
Eric Carlisle <eric@ericcarlisle.com>
2021-10-22 17:03:49 -0400

    Remove repeated object-curly-newline configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,24 +22,17 @@ module.exports = {
     'func-names': 0,
     'prefer-arrow-callback': 0,
     'consistent-return': 0,
-    "object-curly-newline": [
-      "error", 
-      { 
-        "minProperties": 3,
-        "multiline": true,
-      }
-    ],
-    "spaced-comment": 0,
-    "comma-dangle": 0,
-    "semi": 2,
-    "no-prototype-builtins": 0,
-    "object-curly-newline": 0,
-    "no-restricted-syntax": 0,
-    "max-len": 0,
-    "no-plusplus": 0,
-    "no-undef": 0,
-    "arrow-body-style": 0,
-    "no-use-before-define": 0,
-    "radix": 0
+    'spaced-comment': 0,
+    'comma-dangle': 0,
+    semi: 2,
+    'no-prototype-builtins': 0,
+    'object-curly-newline': 0,
+    'no-restricted-syntax': 0,
+    'max-len': 0,
+    'no-plusplus': 0,
+    'no-undef': 0,
+    'arrow-body-style': 0,
+    'no-use-before-define': 0,
+    radix: 0
   }
 };
