af7c4d3a366f97173f3a80356bee762e11cdb23f
Eric Carlisle <eric@ericcarlisle.com>
2021-10-22 18:08:59 -0400

    Make corrections as per ESLint configuration.

--- src/__tests__/query.test.js
+++ src/__tests__/query.test.js
@@ -2,36 +2,35 @@
  * @jest-environment jsdom
  */
 
-import { expandQueryString } from '../util/query.js';
+import { expandQueryString } from '../util/query';
 
 describe('expandQueryString', () => {
+  const saveLocation = global.window.location;
 
-    const saveLocation = global.window.location;
-
-    afterAll(() => {
-        delete global.window.location;
-        global.window.location = saveLocation;
-    });
-
-    it('gets url value for param name passed', () => {
-        delete global.window.location;
-        global.window = Object.create(window);
-        global.window.location = {
-          href:'http://www.test.com?adslot=hello',
-        };
-
-      const result = expandQueryString('adslot');
-      expect(result).toEqual('hello');
-    });
-
-    it('if no result return empty string', () => {
-        delete global.window.location;
-        global.window = Object.create(window);
-        global.window.location = {
-          href:'http://www.test.com?adslot=',
-        };
-
-      const result = expandQueryString('adslot');
-      expect(result).toEqual('');
-    });
+  afterAll(() => {
+    delete global.window.location;
+    global.window.location = saveLocation;
   });
+
+  it('gets url value for param name passed', () => {
+    delete global.window.location;
+    global.window = Object.create(window);
+    global.window.location = {
+      href: 'http://www.test.com?adslot=hello',
+    };
+
+    const result = expandQueryString('adslot');
+    expect(result).toEqual('hello');
+  });
+
+  it('if no result return empty string', () => {
+    delete global.window.location;
+    global.window = Object.create(window);
+    global.window.location = {
+      href: 'http://www.test.com?adslot=',
+    };
+
+    const result = expandQueryString('adslot');
+    expect(result).toEqual('');
+  });
+});
