33a003ea43b58000ef5724a279619745afd6a09e
Eric Carlisle <eric@ericcarlisle.com>
2021-10-22 17:29:20 -0400

    Make corrections as per ESLint configuration.

--- src/__tests__/gpt.test.js
+++ src/__tests__/gpt.test.js
@@ -95,116 +95,115 @@ describe('arcads', () => {
         id: 'div-id-123',
         slotName: 'hp/hp-1',
         adType: 'cube',
-        dimensions: [ [[970, 250], [970, 90], [728, 90]], [[728, 90]], [[320, 100], [320, 50]] ],
+        dimensions: [[[970, 250], [970, 90], [728, 90]], [[728, 90]], [[320, 100], [320, 50]]],
         targeting: {
           section: 'weather'
         },
         sizemap: {
-          breakpoints: [ [1280, 0], [800, 0], [0, 0] ],
+          breakpoints: [[1280, 0], [800, 0], [0, 0]],
           refresh: 'true'
         }
       });
-    
+
       expect(methods.prepareSizeMaps.mock.calls.length).toBe(1);
       expect(methods.setResizeListener.mock.calls.length).toBe(1);
     });
   });
 
-    it('if has prerender that resolves call refresh', () => {
-      window.googletag.pubadsReady = true;
-      const prerenderFnc = jest.fn();
-      gpt.refreshSlot({ad:{name:"ad"}, correlator:false, prerender:prerenderFnc, info:{}});
-      expect(prerenderFnc).toHaveBeenCalledTimes(1);
+  it('if has prerender that resolves call refresh', () => {
+    window.googletag.pubadsReady = true;
+    const prerenderFnc = jest.fn();
+    gpt.refreshSlot({ ad: { name: 'ad' }, correlator: false, prerender: prerenderFnc, info: {} });
+    expect(prerenderFnc).toHaveBeenCalledTimes(1);
+  });
+
+  it('if blockarcAds load is set do not call pubads refresh', () => {
+    window.googletag.pubadsReady = true;
+    window.blockArcAdsLoad = true;
+    const prerenderFnc = jest.fn();
+
+    const refreshMock = jest.fn();
+    global.googletag.pubads = jest.fn().mockReturnValue({
+      refresh: refreshMock,
+    });
+    gpt.refreshSlot({ ad: { name: 'ad' }, correlator: false, prerender: prerenderFnc, info: {} });
+    expect(refreshMock).toHaveBeenCalledTimes(0);
+  });
+
+  describe('setTargeting', () => {
+    it('if options has key and value call ad SetTargeting', () => {
+      const setTargetingMock = jest.fn();
+      const ad = { setTargeting: setTargetingMock };
+      gpt.setTargeting(ad, { testKey: 'testValue' });
+      expect(setTargetingMock).toHaveBeenCalledTimes(1);
+      expect(setTargetingMock).toHaveBeenCalledWith('testKey', 'testValue');
     });
 
-    it('if blockarcAds load is set do not call pubads refresh', () => {
-      window.googletag.pubadsReady = true;
-      window.blockArcAdsLoad = true;
-      const prerenderFnc = jest.fn();
+    it('if options has NOT key and value call ad SetTargeting', () => {
+      const setTargetingMock = jest.fn();
+      const ad = { setTargeting: setTargetingMock };
+      gpt.setTargeting(ad, { testKey: null });
+      expect(setTargetingMock).toHaveBeenCalledTimes(0);
+    });
+  });
 
-      const refreshMock = jest.fn();
+  describe('dfpSettings', () => {
+    const disableInitialLoadMock = jest.fn();
+    const enableSingleRequestMock = jest.fn();
+    const enableAsyncRenderingMock = jest.fn();
+    const enableServicesMock = jest.fn();
+    const collapseEmptyDivsMock = jest.fn();
+    const addEventListenerMock = jest.fn();
+
+    beforeAll(() => {
       global.googletag.pubads = jest.fn().mockReturnValue({
-        refresh: refreshMock,
+        disableInitialLoad: disableInitialLoadMock,
+        enableSingleRequest: enableSingleRequestMock,
+        enableAsyncRendering: enableAsyncRenderingMock,
+        collapseEmptyDivs: collapseEmptyDivsMock,
+        addEventListener: addEventListenerMock,
       });
-      gpt.refreshSlot({ad:{name:"ad"}, correlator:false, prerender:prerenderFnc, info:{}});
-      expect(refreshMock).toHaveBeenCalledTimes(0);
+
+      global.googletag.enableServices = enableServicesMock;
     });
 
-    describe('setTargeting', () => {
-      it('if options has key and value call ad SetTargeting', () => {
-        const setTargetingMock = jest.fn();
-        const ad = {setTargeting: setTargetingMock};
-        gpt.setTargeting(ad, {testKey:"testValue"});
-        expect(setTargetingMock).toHaveBeenCalledTimes(1);
-        expect(setTargetingMock).toHaveBeenCalledWith("testKey","testValue" );
-      });
-
-      it('if options has NOT key and value call ad SetTargeting', () => {
-        const setTargetingMock = jest.fn();
-        const ad = {setTargeting: setTargetingMock};
-        gpt.setTargeting(ad, {testKey:null});
-        expect(setTargetingMock).toHaveBeenCalledTimes(0);
-      });
+    afterEach(() => {
+      jest.clearAllMocks();
     });
 
-    describe('dfpSettings', () => {
-
-      const disableInitialLoadMock = jest.fn();
-        const enableSingleRequestMock = jest.fn();
-        const enableAsyncRenderingMock = jest.fn();
-        const enableServicesMock = jest.fn();
-        const collapseEmptyDivsMock = jest.fn();
-        const addEventListenerMock = jest.fn();
-
-      beforeAll(() => {
-        global.googletag.pubads = jest.fn().mockReturnValue({
-          disableInitialLoad: disableInitialLoadMock,
-          enableSingleRequest : enableSingleRequestMock,
-          enableAsyncRendering: enableAsyncRenderingMock,
-          collapseEmptyDivs: collapseEmptyDivsMock,
-          addEventListener: addEventListenerMock,
-        });
-
-        global.googletag.enableServices = enableServicesMock;
-      });
-
-      afterEach(() => {
-        jest.clearAllMocks();
-      });
-
-      it('call non-logic dependent pubads setup functions', () => {
-        gpt.dfpSettings();
-        expect(disableInitialLoadMock).toHaveBeenCalledTimes(1);
-        expect(enableSingleRequestMock).toHaveBeenCalledTimes(1);
-        expect(enableAsyncRenderingMock).toHaveBeenCalledTimes(1);
-        expect(enableServicesMock).toHaveBeenCalledTimes(1);
-        expect(enableAsyncRenderingMock).toHaveBeenCalledTimes(1);
-        expect(collapseEmptyDivsMock).toHaveBeenCalledTimes(0);
-        expect(addEventListenerMock).toHaveBeenCalledTimes(0);
-      });
-
-      it( 'if  handleSlotRenderEnded function calls addEventListener', () => {
-        const handleMock = jest.fn();
-        gpt.dfpSettings(handleMock);
-        expect(disableInitialLoadMock).toHaveBeenCalledTimes(1);
-        expect(enableSingleRequestMock).toHaveBeenCalledTimes(1);
-        expect(enableAsyncRenderingMock).toHaveBeenCalledTimes(1);
-        expect(enableServicesMock).toHaveBeenCalledTimes(1);
-        expect(enableAsyncRenderingMock).toHaveBeenCalledTimes(1);
-        expect(addEventListenerMock).toHaveBeenCalledTimes(1);
-      }); 
+    it('call non-logic dependent pubads setup functions', () => {
+      gpt.dfpSettings();
+      expect(disableInitialLoadMock).toHaveBeenCalledTimes(1);
+      expect(enableSingleRequestMock).toHaveBeenCalledTimes(1);
+      expect(enableAsyncRenderingMock).toHaveBeenCalledTimes(1);
+      expect(enableServicesMock).toHaveBeenCalledTimes(1);
+      expect(enableAsyncRenderingMock).toHaveBeenCalledTimes(1);
+      expect(collapseEmptyDivsMock).toHaveBeenCalledTimes(0);
+      expect(addEventListenerMock).toHaveBeenCalledTimes(0);
     });
 
-    describe('determineSlotName', () => {
-      it('return slotname based on dfpCode and slotname args', () => {
-        const result = gpt.determineSlotName('dfpCode',"testSlotname");
-        expect(result).toEqual('/dfpCode/testSlotname');
-      });
+    it('if handleSlotRenderEnded function calls addEventListener', () => {
+      const handleMock = jest.fn();
+      gpt.dfpSettings(handleMock);
+      expect(disableInitialLoadMock).toHaveBeenCalledTimes(1);
+      expect(enableSingleRequestMock).toHaveBeenCalledTimes(1);
+      expect(enableAsyncRenderingMock).toHaveBeenCalledTimes(1);
+      expect(enableServicesMock).toHaveBeenCalledTimes(1);
+      expect(enableAsyncRenderingMock).toHaveBeenCalledTimes(1);
+      expect(addEventListenerMock).toHaveBeenCalledTimes(1);
+    });
+  });
 
-      it('ifAdsSlot override then use that value for slotaName', () => {
-        jest.spyOn(queryUtil, 'expandQueryString').mockReturnValue('overrideSlotname');
-        const result = gpt.determineSlotName('dfpCode',"testSlotname");
-        expect(result).toEqual('/dfpCode/overrideSlotname');
-      });
-    });    
+  describe('determineSlotName', () => {
+    it('return slotname based on dfpCode and slotname args', () => {
+      const result = gpt.determineSlotName('dfpCode', 'testSlotname');
+      expect(result).toEqual('/dfpCode/testSlotname');
+    });
+
+    it('ifAdsSlot override then use that value for slotaName', () => {
+      jest.spyOn(queryUtil, 'expandQueryString').mockReturnValue('overrideSlotname');
+      const result = gpt.determineSlotName('dfpCode', 'testSlotname');
+      expect(result).toEqual('/dfpCode/overrideSlotname');
+    });
+  });
 });
